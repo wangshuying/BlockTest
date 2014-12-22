7d15fe05a96dad5cdd07cd1a64f51eb4a247f925
wangshuying <wangshuying@wangshuyingsMBP.mshome.net>
2014-12-22 19:06:03 +0800

    Add test example

--- BlockTest/ViewController.m
+++ BlockTest/ViewController.m
@@ -17,12 +17,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    // test
+    
+    exampleA();
+    examleB();
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+void exampleA() {
+    char a = 'A';
+    ^{
+        printf("%C\n", a);
+    }();
+}
+
+void exampleB_addBlockToArray(NSMutableArray *array) {
+    char b = 'B';
+    [array addObject:^{
+        printf("%C\n", b);
+    }];
+    
+    // without ARC, The code should be:
+    /*
+     [array addObject:[[^{
+     printf("%C\n", b);
+     } copy] autorelease]];
+     */
+}
+
+void examleB() {
+    NSMutableArray *array = [NSMutableArray array];
+    exampleB_addBlockToArray(array);
+    void (^block)() = [array objectAtIndex:0];
+    block();
 }
 
 @end
